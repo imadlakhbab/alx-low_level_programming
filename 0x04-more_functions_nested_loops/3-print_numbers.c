d480c4f371e824eb10fe5ff286e27384b14941e6
imadlakhbab <lakhbabimad@gmail.com>
2023-06-22 18:24:54 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int _putchar(char c);
+/**
+ * print_numbers - 0 to 9
+ * followed by new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
