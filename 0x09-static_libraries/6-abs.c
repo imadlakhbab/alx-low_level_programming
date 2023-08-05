29eddd282c279a086aded09d58ddb03125c40ca5
imadlakhbab <lakhbabimad@gmail.com>
2023-08-04 19:33:22 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - computer the abs value of n
+ * @n: the int to be checked
+ * Return: the abs value
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
