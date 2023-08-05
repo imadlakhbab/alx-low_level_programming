a528942f3fc341ac248e677057875ed851324081
imadlakhbab <lakhbabimad@gmail.com>
2023-08-04 17:00:17 -0700

    add description

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - print nbr of arguments
+ * @argc: number of arg
+ * @argv: array of arg
+ *
+ * Return: 0
+ */
 int main (int argc, char *argv[])
 {
 	(void)argv;
 	printf("%d\n",(argc-1));
-	return 0;
+	return (0);
 }
